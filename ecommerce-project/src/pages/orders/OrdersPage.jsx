9b1317670eed1d6e47aad333a34eb23e84d65db4
zhengshenhao688 <zhengshenhao688@gmail.com>
2025-10-21 16:23:30 +0800

    Separate into smaller components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useState, useEffect, Fragment } from "react";
 import dayjs from "dayjs";
-import { formatMoney } from "../utils/money";
-import { Header } from "../components/Header";
-import BuyAgainIcon from "../assets/images/icons/buy-again.png";
+import { formatMoney } from "../../utils/money";
+import { Header } from "../../components/Header";
+import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
 import "./OrdersPage.css";
 
 export function OrdersPage({ cart }) {
