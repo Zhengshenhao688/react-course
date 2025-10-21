47d0bdaa8d6e7e562680b9a4329d1c3e25a2a41e
zhengshenhao688 <zhengshenhao688@gmail.com>
2025-10-21 17:28:47 +0800

    Separate DeliveryData from OrderSummary

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,31 +1,26 @@
-import dayjs from "dayjs";
 import { DeliveryOptions } from "./DeliveryOptions";
 import { CartItemDetails } from "./CartItemDetails";
+import { DeliveryData } from "./DeliveryData";
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
         cart.map((cartItem) => {
-          const selectedDeliveryOption = deliveryOptions.find(
-            (deliveryOption) => {
-              return deliveryOption.id === cartItem.deliveryOptionId;
-            }
-          );
-
           return (
             <div key={cartItem.productId} className="cart-item-container">
-              <div className="delivery-date">
-                Delivery date:
-                {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
-                  "dddd, MMMM D"
-                )}
-              </div>
+              <DeliveryData
+                deliveryOptions={deliveryOptions}
+                cartItem={cartItem}
+              />
 
               <div className="cart-item-details-grid">
                 <CartItemDetails cartItem={cartItem} />
 
-                <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem}/>
+                <DeliveryOptions
+                  deliveryOptions={deliveryOptions}
+                  cartItem={cartItem}
+                />
               </div>
             </div>
           );
