eff5e55b929ae6cc7fe886e9d3714fa8ddd82262
zhengshenhao688 <zhengshenhao688@gmail.com>
2025-10-22 10:27:46 +0800

    Add cart prop to header in TrackingPage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router";
 import axios from "axios";
+import { Routes, Route } from "react-router";
 import { useEffect, useState } from "react";
+
 import { Homepage } from "./pages/home/HomePage";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+
 import "./App.css";
 
 function App() {
@@ -25,7 +27,7 @@ function App() {
       <Route index element={<Homepage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
-      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="tracking" element={<TrackingPage cart={cart} />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
